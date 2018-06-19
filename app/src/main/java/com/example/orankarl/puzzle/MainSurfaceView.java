e5af39d5a8e952bc7ac4766bdc357d68ce10c8c6
Orankarl <895168349@qq.com>
2018-06-20 02:06:46 +0800

    initialize project

---
+++ app/src/main/java/com/example/orankarl/puzzle/MainSurfaceView.java
@@ -0,0 +1,103 @@
+package com.example.orankarl.puzzle;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainSurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private Context context;
+    private SurfaceHolder holder;
+    private Paint paint;
+    private Thread thread;
+    private Canvas canvas;
+
+    public static int screenW, screenH;
+    private Resources resources = this.getResources();
+    MenuButton buttonStart;
+    boolean flag = true;
+    public MainSurfaceView(Context context) {
+        super(context);
+        this.context = context;
+        holder = this.getHolder();
+        holder.addCallback(this);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        setFocusable(true);
+    }
+
+    private void init() {
+        Bitmap bmpButton = BitmapFactory.decodeResource(resources, R.drawable.button_start);
+        Bitmap bmpButtonPressed = BitmapFactory.decodeResource(resources, R.drawable.button_restart);
+        int posX = MainSurfaceView.screenW/2 - bmpButton.getWidth()/2;
+        int posY = MainSurfaceView.screenH/2 - bmpButton.getHeight()/2;
+        buttonStart = new MenuButton(context, bmpButton, bmpButtonPressed, posX, posY);
+    }
+
+    public void draw() {
+        try {
+            canvas = holder.lockCanvas();
+            if (canvas != null) {
+                canvas.drawColor(Color.WHITE);
+
+                buttonStart.draw(canvas, paint);
+            }
+        } catch (Exception e) {
+
+        } finally {
+            if (canvas != null) holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        buttonStart.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    public void run() {
+        while(flag) {
+            long start = System.currentTimeMillis();
+            draw();
+            long end = System.currentTimeMillis();
+            try {
+                if (end - start < 50) {
+                    Thread.sleep(50 - (end - start));
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        screenW = this.getWidth();
+        screenH = this.getHeight();
+        init();
+        flag = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
---
+++ app/src/main/java/com/example/orankarl/puzzle/MenuButton.java
@@ -0,0 +1,80 @@
+package com.example.orankarl.puzzle;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.prefs.PreferenceChangeEvent;
+
+public class MenuButton {
+    private Context context;
+    private Bitmap bmpBackground, bmpButton, bmpButtonPressed;
+    private int posX, posY;
+    private boolean isPressed;
+    public MenuButton(Context context, Bitmap bmpButton, Bitmap bmpButtonPressed, int x, int y) {
+        this.context = context;
+        this.bmpButton = bmpButton;
+        this.bmpButtonPressed = bmpButtonPressed;
+        this.posX = x;
+        this.posY = y;
+        isPressed = false;
+    }
+    public MenuButton(Bitmap bmpBackground, Bitmap bmpButton, Bitmap bmpButtonPressed, int x, int y) {
+        this.bmpBackground = bmpBackground;
+        this.bmpButton = bmpButton;
+        this.bmpButtonPressed = bmpButtonPressed;
+        this.posX = x;
+        this.posY = y;
+        isPressed = false;
+    }
+
+    public void draw(Canvas canvas, Paint paint) {
+//        canvas.drawBitmap(bmpBackground, 0, 0, paint);
+        if (isPressed) {
+            canvas.drawBitmap(bmpButtonPressed, posX, posY, paint);
+        } else {
+            canvas.drawBitmap(bmpButton, posX, posY, paint);
+        }
+    }
+
+    public void onTouchEvent(MotionEvent event) {
+        // 获取当前触控位置
+        int pointX = (int) event.getX();
+        int pointyY = (int) event.getY();
+
+        // 当用户是按下和移动时
+        if (event.getAction() == MotionEvent.ACTION_DOWN
+                || event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            // 判定用户是否点击按钮
+            if (pointX > posX && pointX < posX + bmpButton.getWidth()) {
+                if (pointyY > posY && pointyY < posY + bmpButton.getHeight()) {
+                    isPressed = true;
+                    Log.d("StartButton", "DOWN");
+                } else {
+                    isPressed = false;
+                }
+            } else {
+                isPressed = false;
+            }
+
+            // 当用于是抬起动作时
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            // 判断抬起时是否点击按钮,防止用户移动到别处
+            if (pointX > posX && pointX < posX + bmpButton.getWidth()) {
+                if (pointyY > posY && pointyY < posY + bmpButton.getHeight()) {
+                    isPressed = false;//抬起后重置 还原Button状态为未按下状态
+                    Log.d("StartButton", "UP");
+                    MainActivity activity = (MainActivity) context;
+                    activity.onButtonPressed();
+                }
+            }
+        }
+
+    }
+}
